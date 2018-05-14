d3c77ae815ad47bf72ef1a64afb60998806bed34
Travis Fischer <fisch0920@gmail.com>
2018-05-14 13:33:05 -0400

    .

--- packages/puppeteer-email-provider-outlook/index.js
+++ packages/puppeteer-email-provider-outlook/index.js
@@ -11,12 +11,42 @@ const signout = require('./lib/signout')
 const sendEmail = require('./lib/send-email')
 const getEmails = require('./lib/get-emails')
 
+/**
+ * Puppeteer email provider for [Outlook](https://outlook.live.com).
+ *
+ * @extends PuppeteerEmailProvider
+ */
 class PuppeteerEmailProviderOutlook extends PuppeteerEmailProvider {
+  /**
+   * Email provider to automate.
+   *
+   * @member {PuppeteerEmailProvider}
+   */
   get name () {
     return 'outlook'
   }
 
-  async signup (user, opts = { }) {
+  /**
+   * Creates a new email account.
+   *
+   * Returns an email session with the authenticated puppeteer browser.
+   *
+   * @param {object} user - User info for the account to create
+   * @param {string} user.username - Username
+   * @param {string} user.password - Password
+   * @param {string} user.firstName - User's given name
+   * @param {string} user.lastName - User's family name
+   * @param {object} user.birthday - User's birthday
+   * @param {string} user.birthday.month - User's birthday month
+   * @param {string} user.birthday.day - User's birthday day
+   * @param {string} user.birthday.year - User's birthday year
+   *
+   * @param {object} opts - Options
+   * @param {Object} opts.browser - Puppeteer browser instance to use
+   *
+   * @return {Promise<PuppeteerEmailSession>}
+   */
+  async signup (user, opts) {
     ow(user, ow.object.plain.nonEmpty)
     ow(user.username, ow.string.nonEmpty)
     ow(user.password, ow.string.nonEmpty)
@@ -35,6 +65,23 @@ class PuppeteerEmailProviderOutlook extends PuppeteerEmailProvider {
     })
   }
 
+  /**
+   * Signs into an existing email account.
+   *
+   * You must specify either `user.username` or `user.email`.
+   *
+   * Returns an email session with the authenticated puppeteer browser.
+   *
+   * @param {object} user - User info for the account to sign into
+   * @param {string} [user.username] - Username (implies email)
+   * @param {string} [user.email] - Email (implies username)
+   * @param {string} user.password - Password
+   *
+   * @param {object} opts - Options
+   * @param {Object} opts.browser - Puppeteer browser instance to use
+   *
+   * @return {Promise<PuppeteerEmailSession>}
+   */
   async signin (user, opts) {
     ow(user, ow.object.plain.nonEmpty)
     ow(user.password, ow.string.nonEmpty)
@@ -64,6 +111,13 @@ class PuppeteerEmailProviderOutlook extends PuppeteerEmailProvider {
     })
   }
 
+  /**
+   * Signs out of an authenticated session.
+   *
+   * @param {PuppeteerEmailSession} session
+   *
+   * @return {Promise}
+   */
   async signout (session) {
     ow(session, ow.object.instanceOf(PuppeteerEmailSession))
 
@@ -72,6 +126,15 @@ class PuppeteerEmailProviderOutlook extends PuppeteerEmailProvider {
     })
   }
 
+  /**
+   * Sends an email from an authenticated session.
+   *
+   * @param {PuppeteerEmailSession} session
+   * @param {object} email - TODO
+   * @param {object} [opts] - Options
+   *
+   * @return {Promise}
+   */
   async sendEmail (session, email, opts = { }) {
     ow(session, ow.object.instanceOf(PuppeteerEmailSession))
     ow(email, ow.object)
@@ -83,6 +146,15 @@ class PuppeteerEmailProviderOutlook extends PuppeteerEmailProvider {
     })
   }
 
+  /**
+   * Fetches emails from the inbox of an authenticated session.
+   *
+   * @param {PuppeteerEmailSession} session
+   * @param {object} [opts] - Options
+   * @param {object} [opts.query] - Search query to narrow down results
+   *
+   * @return {Promise<Object>}
+   */
   async getEmails (session, opts = { }) {
     ow(session, ow.object.instanceOf(PuppeteerEmailSession))
     ow(opts, ow.object.plain)
