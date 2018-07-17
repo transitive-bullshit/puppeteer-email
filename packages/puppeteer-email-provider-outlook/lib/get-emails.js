bfc635fe27e679e3d52988bec06b934b5924bcb1
Travis Fischer <fisch0920@gmail.com>
2018-07-17 01:46:43 -0400

    .

--- packages/puppeteer-email-provider-outlook/lib/get-emails.js
+++ packages/puppeteer-email-provider-outlook/lib/get-emails.js
@@ -14,34 +14,63 @@ module.exports = async (opts) => {
 
   ow(query, ow.string.nonEmpty.label('query'))
 
-  const page = await browser.newPage()
-  await page.goto('https://outlook.live.com/mail/inbox')
+  let page
 
-  // search for an input query to narrow down email results
-  // TODO: don't require a search query
-  await page.waitFor('input[aria-label=Search]', { visible: true })
-  await delay(1000)
-  await page.focus('input[aria-label=Search]')
-  await page.click('input[aria-label=Search]')
-  await page.type('input[aria-label=Search]', query, { delay: 7 })
-  // await page.waitForNavigation({ timeout: 0 })
-  await page.click('button[aria-label=Search]')
-  await delay(4000)
+  try {
+    const page = await browser.newPage()
+    await page.goto('https://outlook.live.com/mail/inbox')
+    await delay(1000)
 
-  const $emails = await page.$$('[data-convid] > div > div')
+    // search for an input query to narrow down email results
+    // TODO: don't require a search query
+    await page.waitFor('input[aria-label=Search]', { visible: true })
+    await delay(2000)
 
-  // fetch and parse individual emails
-  const emails = await pMap($emails, async ($email) => {
-    await Promise.all([
-      page.waitForNavigation(),
-      $email.click()
-    ])
+    let attempts = 0
+    do {
+      await page.focus('input[aria-label=Search]')
+      await page.click('input[aria-label=Search]')
+      await page.type('input[aria-label=Search]', query, { delay: 7 })
 
-    return getEmail(page)
-  }, {
-    concurrency: 1
-  })
+      const value = await page.$eval('input[aria-label=Search]', (el) => el.value)
+      if (value.trim() === query.trim()) {
+        break
+      }
 
-  await page.close()
-  return emails
+      if (++attempts > 3) {
+        throw new Error(`unable to search for query "${query}"`)
+      }
+
+      // erase current input
+      await page.focus('input[aria-label=Search]')
+      for (let i = 0; i < value.length + 8; ++i) {
+        await page.keyboard.press('Backspace')
+      }
+      await delay(200)
+    } while (true)
+
+    // await page.waitForNavigation({ timeout: 0 })
+    await page.click('button[aria-label=Search]')
+    await delay(2000)
+
+    const $emails = await page.$$('[data-convid] > div > div')
+
+    // fetch and parse individual emails
+    const emails = await pMap($emails, async ($email) => {
+      await Promise.all([
+        page.waitForNavigation(),
+        $email.click()
+      ])
+
+      return getEmail(page)
+    }, {
+      concurrency: 1
+    })
+
+    await page.close()
+    return emails
+  } catch (err) {
+    await page.close()
+    throw err
+  }
 }
